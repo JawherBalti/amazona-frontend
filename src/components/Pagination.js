22c5b0f87888d818f8c86a0d3e74ba2fb25af011
jawherbalti <jawher-balti@live.com>
2024-11-08 12:00:17 +0100

    pagination desgin

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,42 +1,56 @@
-import React from 'react'
+import React from "react";
 
-export default function Pagination({ ordersPerPage, totalOrders, paginate, currentPage, maxPageNumberLimit, minPageNumberLimit, handlePrevbtn, handleNextbtn }) {
+export default function Pagination({
+  ordersPerPage,
+  totalOrders,
+  paginate,
+  currentPage,
+  maxPageNumberLimit,
+  minPageNumberLimit,
+  handlePrevbtn,
+  handleNextbtn,
+}) {
+  const pageNumbers = [];
 
-    const pageNumbers = []
+  for (let i = 1; i <= Math.ceil(totalOrders / ordersPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    for (let i = 1; i <= Math.ceil(totalOrders / ordersPerPage); i++) {
-        pageNumbers.push(i)
-    }
-
-    const renderPageNumbers = pageNumbers.map(number => {
-        if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
-            return (
-                <div key={number} className=" page-number">
-                    <li  className={currentPage === number ? "active-page" : ""} onClick={() => paginate(number)}>
-                        {number}
-                    </li>
-                </div>
-            )
-        } else {
-            return null
-        }
-    })
-
-    return (
-        <div>
-            <ul className="row center">
-                <li>
-                    <button onClick={handlePrevbtn} disabled={currentPage === pageNumbers[0] ? true : false}>
-                        Prev
-                    </button>
-                </li>
-                {renderPageNumbers}
-                <li>
-                    <button onClick={handleNextbtn} disabled={currentPage === pageNumbers[pageNumbers.length - 1] ? true : false}>
-                        Next
-                    </button>
-                </li>
-            </ul>
+  const renderPageNumbers = pageNumbers.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+      return (
+        <div
+          onClick={() => paginate(number)}
+          key={number}
+          className={
+            currentPage === number ? "active-page page-number" : "page-number"
+          }
+        >
+          {number}
         </div>
-    )
+      );
+    } else {
+      return null;
+    }
+  });
+
+  return (
+    <div className="row center" style={{ marginTop: "10px" }}>
+      <button
+        onClick={handlePrevbtn}
+        disabled={currentPage === pageNumbers[0] ? true : false}
+      >
+        Prev
+      </button>
+      {renderPageNumbers}
+      <button
+        onClick={handleNextbtn}
+        disabled={
+          currentPage === pageNumbers[pageNumbers.length - 1] ? true : false
+        }
+      >
+        Next
+      </button>
+    </div>
+  );
 }
