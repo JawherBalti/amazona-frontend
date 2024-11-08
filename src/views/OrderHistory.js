c3ce4d7054f111828e9d8a0f49b4c7272e8c4ce8
jawherbalti <jawher-balti@live.com>
2024-11-08 11:58:34 +0100

    fix display bug in order history page

--- src/views/OrderHistory.js
+++ src/views/OrderHistory.js
@@ -1,97 +1,123 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import LoadingBox from '../components/LoadingBox'
-import MessageBox from '../components/MessageBox'
-import { myOrders } from '../actions/order'
-import Pagination from '../components/Pagination'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { myOrders } from "../actions/order";
+import Pagination from "../components/Pagination";
 
 export default function OrderHistory(props) {
+  const myOrderList = useSelector((state) => state.myOrderReducer);
+  const { loading, error, orders } = myOrderList;
+  const userSignIn = useSelector((state) => state.userSignInReducer);
+  const { userInfo } = userSignIn;
 
-    const myOrderList = useSelector(state => state.myOrderReducer)
-    const { loading, error, orders } = myOrderList
-    const userSignIn = useSelector(state => state.userSignInReducer)
-    const { userInfo } = userSignIn
+  const [allMyOrders, setAllMyOrders] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ordersPerPage, setOrdersPerPage] = useState(9);
 
-    const [allMyOrders, setAllMyOrders] = useState([])
-    const [currentPage, setCurrentPage] = useState(1)
-    const [ordersPerPage, setOrdersPerPage] = useState(9)
+  const [pageNumberLimit, setpageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
 
-    const [pageNumberLimit, setpageNumberLimit] = useState(5)
-    const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5)
-    const [minPageNumberLimit, setminPageNumberLimit] = useState(0)
+  const lastOrder = currentPage * ordersPerPage;
+  const firstOrder = lastOrder - ordersPerPage;
 
-    const lastOrder = currentPage * ordersPerPage
-    const firstOrder = lastOrder - ordersPerPage
+  if (!userInfo) {
+    props.history.push("/signin");
+  }
 
-    if (!userInfo) {
-        props.history.push("/signin")
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(myOrders());
+    if (orders) {
+      setAllMyOrders(orders);
     }
+  }, [dispatch]);
 
-    const dispatch = useDispatch()
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    useEffect(() => {
-        dispatch(myOrders())
-        if (orders) {
-            setAllMyOrders(orders)
-        }
-    }, [dispatch])
+  const handleNextbtn = () => {
+    setCurrentPage(currentPage + 1);
 
-    const paginate = (pageNumber) => {
-        setCurrentPage(pageNumber)
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
+  };
 
-    const handleNextbtn = () => {
-        setCurrentPage(currentPage + 1);
+  const handlePrevbtn = () => {
+    setCurrentPage(currentPage - 1);
 
-        if (currentPage + 1 > maxPageNumberLimit) {
-            setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-            setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-        }
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
     }
+  };
 
-    const handlePrevbtn = () => {
-        setCurrentPage(currentPage - 1);
-
-        if ((currentPage - 1) % pageNumberLimit === 0) {
-            setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-            setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-        }
-    }
-
-    return (
-        <div>
-            <div>Order history</div>
-            {loading ? <LoadingBox></LoadingBox> :
-                error ? <MessageBox variant="danger">{error}</MessageBox> :
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Date</th>
-                                <th>Total</th>
-                                <th>Paid</th>
-                                <th>Delivered</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                orders.slice(firstOrder, lastOrder).map(order => (
-                                    <tr key={order._id}>
-                                        <td>{order._id}</td>
-                                        <td>{order.createdAt.substring(0, 10)}</td>
-                                        <td>{order.totalPrice.toFixed(2)}</td>
-                                        <td>{order.isPaid ? order.paidAt.substring(0, 10) : "Not Paid"}</td>
-                                        <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : "Not Delivered"}</td>
-                                        <td>
-                                            <button type="button" className="small" onClick={() => { props.history.push(`/order/${order._id}`) }}>Details</button>
-                                        </td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-            }
-            <Pagination ordersPerPage={ordersPerPage} totalOrders={orders ? orders.length : allMyOrders.length} paginate={paginate} currentPage={currentPage} maxPageNumberLimit={maxPageNumberLimit} minPageNumberLimit={minPageNumberLimit} handleNextbtn={handleNextbtn} handlePrevbtn={handlePrevbtn}></Pagination>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Order history</h1>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Paid</th>
+              <th>Delivered</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.slice(firstOrder, lastOrder).map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{order.totalPrice.toFixed(2)}</td>
+                <td>
+                  {order.isPaid && order.paidAt
+                    ? order.paidAt?.substring(0, 10)
+                    : "Not Paid"}
+                </td>
+                <td>
+                  {order.isDelivered && order.deliveredAt
+                    ? order.deliveredAt?.substring(0, 10)
+                    : "Not Delivered"}
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => {
+                      props.history.push(`/order/${order._id}`);
+                    }}
+                  >
+                    Details
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <Pagination
+        ordersPerPage={ordersPerPage}
+        totalOrders={orders ? orders.length : allMyOrders.length}
+        paginate={paginate}
+        currentPage={currentPage}
+        maxPageNumberLimit={maxPageNumberLimit}
+        minPageNumberLimit={minPageNumberLimit}
+        handleNextbtn={handleNextbtn}
+        handlePrevbtn={handlePrevbtn}
+      ></Pagination>
+    </div>
+  );
 }
