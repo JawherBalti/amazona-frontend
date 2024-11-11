eb12d3453c0e4fd1fd3047abdc2855c37c8f440b
jawherbalti <jawher-balti@live.com>
2024-11-11 15:33:46 +0100

    add create products page

---
+++ src/views/CreateProduct.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { useSelector } from "react-redux";
+import { api } from "..";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    case "UPDATE_REQUEST":
+      return { ...state, loadingUpdate: true };
+    case "UPDATE_SUCCESS":
+      return { ...state, loadingUpdate: false };
+    case "UPDATE_FAIL":
+      return { ...state, loadingUpdate: false };
+      case 'UPLOAD_REQUEST':
+        return { ...state, loadingUpload: true, errorUpload: '' };
+      case 'UPLOAD_SUCCESS':
+        return {
+          ...state,
+          loadingUpload: false,
+          errorUpload: '',
+        };
+      case 'UPLOAD_FAIL':
+        return { ...state, loadingUpload: false, errorUpload: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default function CreateProduct(props) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState("");
+  const [brand, setBrand] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  const userSignIn = useSelector((state) => state.userSignInReducer);
+  const { userInfo } = userSignIn;
+
+  const [{ loading, error, loadingUpdate, loadingUpload }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append('file', file);
+    try {
+      dispatch({ type: 'UPLOAD_REQUEST' });
+      const { data } = await api.post('/api/upload', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          authorization: `Bearer ${userInfo.data.token}`,
+        },
+      });
+      dispatch({ type: 'UPLOAD_SUCCESS' });
+      setImage(data.secure_url);
+    } catch (err) {
+        console.log(err);
+      dispatch({ type: 'UPLOAD_FAIL', payload: err });
+    }
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch({ type: "UPDATE_REQUEST" });
+      await api.post(
+        `/api/products/create`,
+        {
+          name,
+          price,
+          image,
+          category,
+          brand,
+          countInStock,
+          description,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.data.token}` },
+        }
+      );
+      dispatch({
+        type: "UPDATE_SUCCESS",
+      });
+      props.history.push("/products");
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
+  return (
+    <div className="small-container">
+      <form className="form" onSubmit={submitHandler}>
+        <div className="mb-3" controlId="name">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3" controlId="name">
+          <label htmlFor="price">Price</label>
+          <input
+            id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3" controlId="image">
+          <label htmlFor="image">Image File</label>
+          <input
+              id="image"
+              type="file"
+              onChange={uploadFileHandler}
+            />
+        </div>
+        <div className="mb-3" controlId="category">
+          <label htmlFor="category">Category</label>
+          <input
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3" controlId="brand">
+          <label htmlFor="brand">Brand</label>
+          <input
+            id="brand"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3" controlId="countInStock">
+          <label htmlFor="count">Count In Stock</label>
+          <input
+            id="count"
+            value={countInStock}
+            onChange={(e) => setCountInStock(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3" controlId="description">
+          <label htmlFor="description">Description</label>
+          <input
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <button className="primary" type="submit">
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
