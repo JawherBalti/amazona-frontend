34ebb493010a0aa9fc70b411f09c70d5c28a0d4a
jawherbalti <jawher-balti@live.com>
2024-11-14 11:24:59 +0100

    fix svg properties

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -17,11 +17,11 @@ export default function Rating(props) {
             space="preserve"
             fill="#000000"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
