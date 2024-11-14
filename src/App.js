0161de59085801b67749fe72f1e002b7ba40dab2
jawherbalti <jawher-balti@live.com>
2024-11-14 11:02:52 +0100

    redirection to signin page after signout

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
                   <Link to="/orderhistory">Order History</Link>
                 </li>
                 <li>
-                  <Link to="#signout" onClick={signoutHandler}>
+                  <Link to="/signin" onClick={signoutHandler}>
                     Sign Out
                   </Link>
                 </li>
